dfdb81cae06ee1e5a2929ff3a1d2130a0f9c7712
Salman Ahamed <salmanahamed5900@gmail.com>
2025-05-08 22:23:39 +0530

    changes 7

--- myproject2/settings.py
+++ myproject2/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'widget_tweaks',
     'crispy_forms',
     'notesEngine',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
